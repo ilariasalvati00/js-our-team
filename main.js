a3530abe9c61da2dfb7827b629fed34735884f69
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-11-12 23:39:11 +0100

    Milestone 0 e 1 fatti

---
+++ main.js
@@ -0,0 +1,53 @@
+const membri = [];
+
+const membro1 ={
+    "nome" : 'Wayne Barnett',
+    "ruolo": 'Founder & CEO',
+    "foto" : 'img/wayne-barnett-founder-ceo.jpg'
+};
+
+membri.push(membro1);
+
+const membro2 ={
+    "nome" : 'Angela Caroll',
+    "ruolo": 'Chief Editor',
+    "foto" : 'img/angela-caroll-chief-editor.jpg'
+};
+
+membri.push(membro2);
+
+const membro3 ={
+    "nome" : 'Walter Gordon',
+    "ruolo": 'Office Manager',
+    "foto" : 'img/walter-gordon-office-manager.jpg'
+};
+
+membri.push(membro3);
+
+const membro4 ={
+    "nome" : 'Angela Lopez',
+    "ruolo": 'Social Media Manager',
+    "foto" : 'img/angela-lopez-social-media-manager.jpg'
+};
+
+membri.push(membro4);
+
+const membro5 ={
+    "nome" : 'Scott Estrada',
+    "ruolo": 'Developer',
+    "foto" : 'img/scott-estrada-developer.jpg'
+};
+
+membri.push(membro5);
+
+const membro6 ={
+    "nome" : 'Barbara Ramos',
+    "ruolo": 'Graphic Designer',
+    "foto" : 'img/barbara-ramos-graphic-designer.jpg'
+};
+
+membri.push(membro6);
+
+console.log(membri);
+
+
